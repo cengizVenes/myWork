93b4e4b512184b28610f72e7218c7c5aadc3ff0d
cengizVenes <cengiz.v.enes@gmail.com>
2023-08-24 13:27:33 +0300

    mesasge

--- Carousel-1/style.js
+++ Carousel-1/style.js
@@ -6,25 +6,3 @@ document.addEventListener("mousemove", (e) => {
     takipCismi.style.left = imlecX + "px";
     takipCismi.style.top = imlecY + "px";
 });
-function boyama() {
-    var
-        canvas = document.querySelector('canvas'),
-        ctx = canvas.getContext('2d'),
-        beginPath = false;
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    document.body.addEventListener('mousemove', function (event) {
-        var x = event.clientX, y = event.clientY;
-
-        if (beginPath) {
-            ctx.lineTo(x, y);
-            ctx.stroke();
-        } else {
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            beginPath = true;
-        }
-    }, false);
-}
